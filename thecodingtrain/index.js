64ebeb5055480c7b3af5a9b9b0f92f6041f088f3
Paul Tobin <tobster111@gmail.com>
2019-06-10 22:13:06 +0100

    Sum function refactored for a single line

--- thecodingtrain/index.js
+++ thecodingtrain/index.js
@@ -10,9 +10,30 @@
 // console.log(tripler(4));
 
 // map()
-let vals = [4, 8, 1, 2, 9];
-console.log(vals);
+// let vals = [4, 8, 1, 2, 9];
+// console.log(vals);
 
-const doubled = vals.map(x => x * 2);
+// const doubled = vals.map(x => x * 2);
 
-console.log(doubled);
+// console.log(doubled);
+
+// reduce()
+let vals = [5, 4, 1, 2, 9];
+let acc = 0;
+for (let val of vals) {
+  acc += val;
+}
+
+// console.log(acc);
+
+function sum(acc, val) {
+  console.log(acc);
+  return acc + val;
+}
+
+(acc, val) => acc + val;
+
+let result = vals.reduce((acc, val) => acc + val, 0);
+console.log(result);
+
+
