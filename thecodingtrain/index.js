dc22918c70a2b25cdc2cf1c43b66282afe6c5613
Paul Tobin <tobster111@gmail.com>
2019-06-10 14:12:03 +0100

    doubler function using map()

--- thecodingtrain/index.js
+++ thecodingtrain/index.js
@@ -1,9 +1,21 @@
-const multiplier = factor => {
-  return x => x * factor;
-};
+// higher order functions - functions that take functions or return functions
+// const multiplier = factor => {
+//   return x => x * factor;
+// };
 
-const doubler = multiplier(2);
-const tripler = multiplier(3);
+// const doubler = multiplier(2);
+// const tripler = multiplier(3);
 
-console.log(doubler(4));
-console.log(tripler(4));
+// console.log(doubler(4));
+// console.log(tripler(4));
+
+// map()
+let vals = [4, 8, 1, 2, 9];
+console.log(vals);
+
+function doubler(x) {
+  return x * 2;
+}
+
+const doubled = vals.map(doubler);
+console.log(doubled);
