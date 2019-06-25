25f642c5ca345adaf57d6ed24199461ceb7f99ed
Paul Tobin <tobster111@gmail.com>
2019-06-25 15:38:56 +0100

    Adding the colours to an array and using a for loop to call the function

--- JamesSinclair/Part2.js
+++ JamesSinclair/Part2.js
@@ -1,7 +1,7 @@
 // ! Working with arrays and lists
-// Recall that in the previous article, we talked about DRY code. 
+// Recall that in the previous article, we talked about DRY code.
 // We saw that functions are useful for bundling up sets of actions
-// that might be repeated. But what if we’re repeating the same 
+// that might be repeated. But what if we’re repeating the same
 // function lots of times? For example:
 
 function addColour(colour) {
@@ -18,3 +18,13 @@ addColour('yellow');
 addColour('green');
 addColour('blue');
 addColour('purple');
+
+// That addColour function is called rather a lot. We are repeating ourselves—something
+// we wish to avoid. One way to refactor it is to move the list of colours into an array,
+// and call addColour in a for-loop:
+
+var colours = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'];
+
+for (var i = 0; i < colours.length; i = i + 1) {
+  addColour(colours[i]);
+}
