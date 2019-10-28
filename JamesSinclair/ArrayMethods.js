da2207f331cdf265c4888a50d7646bd1b2e2f542
Paul Tobin <tobster111@gmail.com>
2019-10-28 14:57:06 +0000

    Playing around with Array Methods .find()

---
+++ JamesSinclair/ArrayMethods.js
@@ -0,0 +1,31 @@
+const heroes = [
+  { name: 'Hulk', strength: 50000 },
+  { name: 'Spider-Man', strength: 25000 },
+  { name: 'Hawk Eye', strength: 136 },
+  { name: 'Thor', strength: 100000 },
+  { name: 'Black Widow', strength: 136 },
+  { name: 'Vision', strength: 5000 },
+  { name: 'Scarlet Witch', strength: 60 },
+  { name: 'Mystique', strength: 120 },
+  { name: 'Namora', strength: 75000 },
+  { name: 'Captain America', strength: 362 },
+  { name: 'Deadpool', strength: 1814 },
+  { name: 'Black Panther', strength: 1814 }
+];
+
+//* .find()
+function isHulk(hero) {
+  return hero.name === 'Hulk';
+}
+
+const hulk = heroes.find(isHulk); 
+
+const result = heroes
+  .map(hero => {
+    return { name: hero.name, strength: hero.strength };
+  })
+  .filter(name => {
+    return name.name === 'Hulk';
+  });
+
+console.log(hulk);
