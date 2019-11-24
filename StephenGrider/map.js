01518535ac4e5cc983e93372e6be4a8b94c3c417
Paul Tobin <tobster111@gmail.com>
2019-11-24 19:28:55 +0000

    map() - same result using .map()

--- StephenGrider/map.js
+++ StephenGrider/map.js
@@ -6,4 +6,8 @@ for (let i = 0; i < numbers.length; i++) {
   doubleNumbers.push(numbers[i] * 2);
 }
 
+const doubled = numbers.map(number => number * 2);
+
 console.log(doubleNumbers);
+
+console.log(doubled);
