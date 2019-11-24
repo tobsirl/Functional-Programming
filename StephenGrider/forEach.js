e7a15a4bda56d57a124c006ccf54dd61eafabb53
Paul Tobin <tobster111@gmail.com>
2019-11-24 15:21:04 +0000

    Complete Coding Exercise 1

--- StephenGrider/forEach.js
+++ StephenGrider/forEach.js
@@ -23,3 +23,17 @@ numbers.forEach(adder);
 
 // Print the sum variable
 console.log(sum);
+
+// Coding Exercise 1 - Moving away from For Loops
+function handlePosts() {
+  var posts = [
+    { id: 23, title: 'Daily JS News' },
+    { id: 52, title: 'Code Refactor City' },
+    { id: 105, title: 'The Brightest Ruby' }
+  ];
+
+  // for (var i = 0; i < posts.length; i++) {
+  //   savePost(posts[i]);
+  // }
+  posts.forEach(post => savePost(post));
+}
