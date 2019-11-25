e54726dc80d009acb9d57a50a78cea73c664d007
Paul Tobin <tobster111@gmail.com>
2019-11-25 19:16:20 +0000

    Coding Exercise 2 - complete

--- StephenGrider/map.js
+++ StephenGrider/map.js
@@ -31,3 +31,14 @@ var images = [
 var heights = images.map(image => image.height);
 
 console.log(heights);
+
+// Coding Exercise 1 - Calculating Values with Map
+var trips = [
+  { distance: 34, time: 10 },
+  { distance: 90, time: 50 },
+  { distance: 59, time: 25 }
+];
+
+var speeds = trips.map(trip => trip.distance / trip.time);
+
+console.log(speeds);
