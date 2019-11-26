526aa4f692768edd4c1c9bdd67424e1c45bdcfaf
Paul Tobin <tobster111@gmail.com>
2019-11-26 17:20:37 +0000

    Example of using filter to get fruit products from an array

---
+++ StephenGrider/filter.js
@@ -0,0 +1,10 @@
+const products = [
+  { name: 'cucumber', type: 'vegetable' },
+  { name: 'banana', type: 'fruit' },
+  { name: 'celery', type: 'vegetable' },
+  { name: 'orange', type: 'fruit' }
+];
+
+const fruits = products.filter(fruit => fruit.type === 'fruit');
+
+console.log(fruits);
