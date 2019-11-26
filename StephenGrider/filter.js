5fa8b56da71710210661feed85daa4a7a815fb1c
Paul Tobin <tobster111@gmail.com>
2019-11-26 17:53:04 +0000

    Example of using relational data to return the proper result

--- StephenGrider/filter.js
+++ StephenGrider/filter.js
@@ -32,3 +32,17 @@ const complexFilter = products.filter(
 );
 
 console.log(complexFilter);
+
+const post = { id: 4, title: 'New Post' };
+
+const comments = [
+  { postId: 4, content: 'awesome post' },
+  { postId: 3, content: 'it was ok' },
+  { postId: 4, content: 'neat' }
+];
+
+const commentsForPost = (post, comments) => {
+  return comments.filter(comment => comment.postId === post.id);
+};
+
+console.log(commentsForPost(post, comments));
