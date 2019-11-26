c9d59d880218e2661674602b3ca5751f29fa9cbe
Paul Tobin <tobster111@gmail.com>
2019-11-26 17:43:46 +0000

    More complex example of the filter method

--- StephenGrider/filter.js
+++ StephenGrider/filter.js
@@ -1,8 +1,8 @@
 const products = [
-  { name: 'cucumber', type: 'vegetable' },
-  { name: 'banana', type: 'fruit' },
-  { name: 'celery', type: 'vegetable' },
-  { name: 'orange', type: 'fruit' }
+  { name: 'cucumber', type: 'vegetable', quantity: 0, price: 1 },
+  { name: 'banana', type: 'fruit', quantity: 10, price: 15 },
+  { name: 'celery', type: 'vegetable', quantity: 30, price: 9 },
+  { name: 'orange', type: 'fruit', quantity: 3, price: 5 }
 ];
 
 const filteredProducts = [];
@@ -21,6 +21,14 @@ for (fruit of products) {
 
 console.log(filteredProducts);
 
-const fruits = products.filter(fruit => fruit.type === 'fruit');
+const fruits = products.filter(product => product.type === 'fruit');
 
 console.log(fruits);
+
+// Type is 'vegetable' quantity is greater than 0, price is less than 10
+const complexFilter = products.filter(
+  product =>
+    product.type === 'vegetable' && product.quantity > 0 && product.price < 10
+);
+
+console.log(complexFilter);
