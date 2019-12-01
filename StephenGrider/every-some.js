ec9d1f7de2f9515740b75afed1d2f9c79006307a
Paul Tobin <tobster111@gmail.com>
2019-12-01 11:49:13 +0000

    Example of using every and some methods, writing out the code in and imperative manner

---
+++ StephenGrider/every-some.js
@@ -0,0 +1,21 @@
+const computers = [
+  { name: 'Apple', ram: 24 },
+  { name: 'Compaq', ram: 4 },
+  { name: 'Acer', ram: 32 }
+];
+
+let allComputersCanRunProgram = true;
+let onlySomeComputersCanRunProgram = false;
+
+for (let i = 0; i < computers.length; i++) {
+  let computer = computers[i];
+
+  if (computer.ram < 16) {
+    allComputersCanRunProgram = false;
+  } else {
+    onlySomeComputersCanRunProgram = true;
+  }
+}
+
+allComputersCanRunProgram
+onlySomeComputersCanRunProgram
