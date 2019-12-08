4cc73c44573c2d0f48131bc9f7b8e12e983933db
Paul Tobin <tobster111@gmail.com>
2019-12-08 12:14:07 +0000

    Arrow Functions
    ES6 version, arrow funciton are lexically scopped

--- StephenGrider/ArrowFunctions.js
+++ StephenGrider/ArrowFunctions.js
@@ -7,3 +7,16 @@
 const double = number => 2 * number;
 
 double(8);
+
+// ES6
+const team = {
+  members: ['Jane', 'Bill'],
+  teamName: 'Super Squad',
+  teamSummary: function() {
+    return this.members.map(member => {
+      return `${member} is on team ${this.teamName}`;
+    });
+  }
+};
+
+team.teamSummary(); //?
