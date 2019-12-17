8fbdb0397cd56140b3770e691a13200fbc3480f6
Paul Tobin <tobster111@gmail.com>
2019-12-17 20:17:50 +0000

    Rest and Spread
    Example of using the spread operator to join arrays together

--- StephenGrider/RestAndSpread.js
+++ StephenGrider/RestAndSpread.js
@@ -1,5 +1,20 @@
+// Rest
 function addNumbers(...numbers) {
   return numbers.reduce((sum, number) => sum + number);
 }
 
 addNumbers(1, 2, 3, 4, 5, 6); //?
+
+// Spread
+const defaultColors = ['red', 'green'];
+const userFavoriteColors = ['orange', 'yellow'];
+const fallColors = ['fire red', 'fall orange'];
+
+defaultColors.concat(userFavoriteColors); //?
+
+const newArray = [
+  'blue',
+  ...fallColors,
+  ...defaultColors,
+  ...userFavoriteColors
+]; //?
