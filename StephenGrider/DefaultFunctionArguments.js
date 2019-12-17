a097d58e779fe848f2181f7fad5861adb3e35eaa
Paul Tobin <tobster111@gmail.com>
2019-12-17 19:44:35 +0000

    Default Function Arguments
    Coding Exercise 2 - complete

--- StephenGrider/DefaultFunctionArguments.js
+++ StephenGrider/DefaultFunctionArguments.js
@@ -7,3 +7,10 @@ function makeAjaxRequest(url, method = 'GET') {
 function sum(a = 0, b = 0) {
   return a + b;
 }
+
+// Coding Exercise 2 - Dumping Unused Code
+function addOffset(style = {}) {
+  style.offset = '10px';
+
+  return style;
+}
