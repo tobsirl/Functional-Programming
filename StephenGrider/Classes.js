6b116033f2611dcd15e982203041ad348f07163b
Paul Tobin <tobster111@gmail.com>
2020-02-17 17:32:00 +0000

    Classes - example of the boilerplate code required to inherit from the parent class in javascript

--- StephenGrider/Classes.js
+++ StephenGrider/Classes.js
@@ -6,6 +6,23 @@ Car.prototype.drive = function() {
   return 'vroom';
 };
 
+function Toyota(options) {
+  Car.call(this, options);
+  this.color = options.color;
+}
+
+Toyota.prototype = Object.create(Car.prototype);
+Toyota.prototype.constructor = Toyota;
+
+Toyota.prototype.honk = function() {
+  return 'honk';
+};
+
 const car = new Car({ title: 'Focus' });
 car.drive(); //?
 car;
+
+const toyota = new Toyota({ color: 'red', title: 'Daily Driver' });
+toyota;
+toyota.drive(); //?
+toyota.honk(); //?
