4b63004d16bb1e091158630fd07dd2c50b446771
Paul Tobin <tobster111@gmail.com>
2020-02-17 18:01:04 +0000

    Classes - example of ES6 with constructor and method

--- StephenGrider/Classes.js
+++ StephenGrider/Classes.js
@@ -1,28 +1,44 @@
-function Car(options) {
-  this.title = options.title;
+// function Car(options) {
+//   this.title = options.title;
+// }
+
+// Car.prototype.drive = function() {
+//   return 'vroom';
+// };
+
+// function Toyota(options) {
+//   Car.call(this, options);
+//   this.color = options.color;
+// }
+
+// Toyota.prototype = Object.create(Car.prototype);
+// Toyota.prototype.constructor = Toyota;
+
+// Toyota.prototype.honk = function() {
+//   return 'honk';
+// };
+
+// const car = new Car({ title: 'Focus' });
+// car.drive(); //?
+// car;
+
+// const toyota = new Toyota({ color: 'red', title: 'Daily Driver' });
+// toyota;
+// toyota.drive(); //?
+// toyota.honk(); //?
+
+// ES6
+class Car {
+  constructor({ title }) {
+    this.title = title;
+  }
+
+  drive() {
+    return 'vroom';
+  }
 }
 
-Car.prototype.drive = function() {
-  return 'vroom';
-};
+const car = new Car({ title: 'Ford' });
 
-function Toyota(options) {
-  Car.call(this, options);
-  this.color = options.color;
-}
-
-Toyota.prototype = Object.create(Car.prototype);
-Toyota.prototype.constructor = Toyota;
-
-Toyota.prototype.honk = function() {
-  return 'honk';
-};
-
-const car = new Car({ title: 'Focus' });
-car.drive(); //?
 car;
-
-const toyota = new Toyota({ color: 'red', title: 'Daily Driver' });
-toyota;
-toyota.drive(); //?
-toyota.honk(); //?
+car.drive(); //?
